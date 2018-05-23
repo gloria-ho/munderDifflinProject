0f6b36fe7034bfb5ee5330d50802cf59a942d6de
silverdragonia <silverdragonia@gmail.com>
2018-05-23 17:55:52 -0400

    updating behavior, style on css and js

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 'use strict';
 
-// join us alert & redirect
-let joinButton = document.querySelector('#joinButton');
-let joinUs = document.querySelector('#joinUs');
+$(document).ready(function(){
+  // toggles classes on menu icon 
+  animateIcon();
 
-joinButton.addEventListener('click', function() {
-	event.preventDefault();
-	alert('Thank you for your interest! Please send us a message and we will get back to you shortly.');
-	window.location.href = '#contact';
-	return false;
+
+  //hamburger menu animation
+  function animateIcon() {
+    $('.animated-icon').click(function(){
+        $(this).toggleClass('open');
+        $('#collapsableNavBar').toggleClass('collapse');
+    });
+  }
+
+  // contact button & alert
+  function contactButtonNotify() {
+    $('#contactForm').submit(function(e) {
+      var userName = $('#userName').val();
+      alert(`Thank you for your message, ${userName}. Someone will get back to you within 1-2 business days. `);
+      $('#contactForm').hide();
+      $('#messageConfirmation').show();
+      return false;
+    });
+  }
+
+  // join us alert & redirect to #contact
+  function joinButtonNotify() {
+    $('#joinButton').click(function(e) {
+      alert('Thank you for your interest! Please send us a message and we will get back to you shortly');
+      window.location.href = '#contact';
+    });
+  }
+
+  // // email subscribe button & alert
+  function subscribeButton() {
+    $('#emailForm').submit(function(e) {
+      $('#subscribeButton').removeClass('btn-outline-secondary')
+      .addClass('btn-success')
+      .html('<i class="far fa-check-square"></i>');
+      var userEmail = $('#emailInput').val();
+      alert(`${userEmail} has been subscribed, thank you!`);
+      return false;
+      });
+  }
+
+  contactButtonNotify();
+  joinButtonNotify();
+  subscribeButton()
+  
 })
-
-// contact button
-let contactButton = document.querySelector('#sendMessage');
-let contactForm = document.querySelector('#contactForm');
-let messageConfirm = document.querySelector('#messageConfirmation');
-
-messageButton.addEventListener('click', function() {
-	event.preventDefault();
-	let userName = document.querySelector('#userName').value;
-	alert(`Thank you for your message, ${userName}. Someone will get back to you within 1-2 business days.`);
-	contactForm.style.display = 'none';
-	messageConfirm.style.display = 'visible';
-	return false;
-})
-
-// email button
-let emailButton = document.querySelector('#subscribeButton');
-
-emailButton.addEventListener('click', function() {
-	event.preventDefault();
-	let userEmail = document.querySelector('#emailInput').value;
-	alert(`${userEmail} has been subscribed, thank you!`);
-	return false;
-})`
